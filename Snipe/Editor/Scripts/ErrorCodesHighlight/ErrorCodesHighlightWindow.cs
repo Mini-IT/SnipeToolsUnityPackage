51c9f4bf9fd8a8e66d5aae2cd95059736794e1f4
gluks <gluk-02@yandex.ru>
2024-12-18 18:00:26 +0500

    Update ErrorCodesHighlightWindow.cs

--- Snipe/Editor/Scripts/ErrorCodesHighlight/ErrorCodesHighlightWindow.cs
+++ Snipe/Editor/Scripts/ErrorCodesHighlight/ErrorCodesHighlightWindow.cs
@@ -8,17 +8,31 @@ namespace MiniIT.Snipe.Unity.Editor
 	public class ErrorCodesHighlightWindow : EditorWindow
 	{
 		private ErrorCodesTracker _tracker;
+		private Dictionary<string, List<string>> _groupedMessages;
 
 		//[MenuItem("Snipe/ErrorCodes...")]
 		public static void ShowWindow(ErrorCodesTracker tracker)
 		{
-			var window = EditorWindow.GetWindow<ErrorCodesHighlightWindow>("Snipe ErrorCodes");
+			var window = EditorWindow.GetWindow<ErrorCodesHighlightWindow>(true, "Snipe ErrorCodes", true);
 			window.Init(tracker);
 		}
 
 		private void Init(ErrorCodesTracker tracker)
 		{
 			_tracker = tracker;
+
+			_groupedMessages = new Dictionary<string, List<string>>(_tracker.Items.Count);
+			foreach (var item in _tracker.Items)
+			{
+				if (item.TryGetValue("message_type", out var msgtype) && msgtype is string messageType)
+				{
+					if (!_groupedMessages.ContainsKey(messageType))
+					{
+						_groupedMessages.Add(messageType, new List<string>());
+					}
+					_groupedMessages[messageType].Add(fastJSON.JSON.ToJSON(item));
+				}
+			}
 		}
 
 		private void OnGUI()
@@ -30,10 +44,15 @@ namespace MiniIT.Snipe.Unity.Editor
 
 			EditorGUILayout.BeginVertical();
 
-			for (int i = 0; i < _tracker.Items.Count; i++)
+			foreach (var msg in _groupedMessages)
 			{
-				IDictionary<string, object> item = _tracker.Items[i];
-				EditorGUILayout.TextField(fastJSON.JSON.ToJSON(item));
+				EditorGUILayout.Foldout(true, $"{msg.Key} ({msg.Value.Count})", true);
+				EditorGUI.indentLevel++;
+				foreach (string item in msg.Value)
+				{
+					EditorGUILayout.TextField(item);
+				}
+				EditorGUI.indentLevel++;
 			}
 
 			EditorGUILayout.EndVertical();
