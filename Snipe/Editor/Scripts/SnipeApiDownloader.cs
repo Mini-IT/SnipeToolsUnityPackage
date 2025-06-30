197534e6a555db9e68fba3b9fa16e07aac1f4b0c
gluks <gluk-02@yandex.ru>
2025-06-30 14:43:30 +0200

    Update SnipeApiDownloader.cs

--- Snipe/Editor/Scripts/SnipeApiDownloader.cs
+++ Snipe/Editor/Scripts/SnipeApiDownloader.cs
@@ -14,7 +14,7 @@ namespace MiniIT.Snipe.Unity.Editor
 	{
 #if SNIPE_8_0_OR_NEWER
 		private const string SNIPE_VERSION_SUFFIX = "V8";
-#if SNIPE_7_1_OR_NEWER
+#elif SNIPE_7_1_OR_NEWER
 		private const string SNIPE_VERSION_SUFFIX = "V71";
 #elif SNIPE_7_0_OR_NEWER
 		private const string SNIPE_VERSION_SUFFIX = "V7";
